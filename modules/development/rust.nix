c5ee9711e352e1c9f8ef6befcef2f624e6dc0db8
CodyBense <codybense@gmail.com>
2026-01-16 10:59:13 -0500

    formatting

--- modules/development/rust.nix
+++ modules/development/rust.nix
@@ -1,11 +1,11 @@
 { config, lib, pkgs, ... }:
 
 {
-	environment.systemPackages = with pkgs; [
-		rustc
-		cargo
-		rustfmt
-		rust-analyzer
-		clippy
-	];
+  environment.systemPackages = with pkgs; [
+    rustc
+    cargo
+    rustfmt
+    rust-analyzer
+    clippy
+  ];
 }
