2e39f17a6a8ebbc10b6be18ebb5bfaa5d353e134
CodyBense <codybense@gmail.com>
2026-01-18 17:18:59 -0500

    updated flake to include pikachu machine

--- flake.nix
+++ flake.nix
@@ -7,12 +7,12 @@
 
     home-manager = {
       url = "github:nix-community/home-manager";
-	  inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     sops-nix = {
       url = "github:Mic92/sops-nix";
-	  inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     zen-browser = {
@@ -22,42 +22,64 @@
 
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
-	  inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = { self, nixpkgs, home-manager, ... } @inputs:
   let
-  	system = "x86_64-linux";
-	username = "cody";
-	overlays = [
-	];
+    system = "x86_64-linux";
+    username = "cody";
+    overlays = [
+    ];
   in
     {
-  	  nixosConfigurations = {
-		"revan" = nixpkgs.lib.nixosSystem {
-		  specialArgs = {
-			inherit system;
-			inherit inputs;
-			inherit username;
-		  };
-		  modules = [
-			./hosts/revan/configuration.nix
-			inputs.sops-nix.nixosModules.sops
-			./modules/desktop/noctalia.nix
-			home-manager.nixosModules.home-manager
-			{
-			  home-manager.extraSpecialArgs = {
-				inherit inputs;
-				inherit username;
-			  };
-			  home-manager.useGlobalPkgs = true;
-			  home-manager.useUserPackages = true;
-			  home-manager.backupFileExtension = "backup";
-			  home-manager.users."cody" = ./modules/home-manager;
-			}
-		  ];
-		};
-	  };
+      nixosConfigurations = {
+        "revan" = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit system;
+            inherit inputs;
+            inherit username;
+          };
+          modules = [
+            ./hosts/revan/configuration.nix
+            inputs.sops-nix.nixosModules.sops
+            ./modules/desktop/noctalia.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.extraSpecialArgs = {
+                inherit inputs;
+                inherit username;
+              };
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "backup";
+              home-manager.users."cody" = ./modules/home-manager;
+            }
+          ];
+        };
+      };
+      nixosConfigurations = {
+        "pikachu" = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit system;
+            inherit inputs;
+          };
+          modules = [
+            ./hosts/pikachu/configuration.nix
+            inputs.sops-nix.nixosModules.sops
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.extraSpecialArgs = {
+                inherit inputs;
+              };
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "backup";
+              home-manager.users."codybense" = ./modules/home-manager;
+            }
+          ];
+        };
+      };
     };
 }
