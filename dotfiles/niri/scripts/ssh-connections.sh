f4fbe17f6e03510c971d4dbcff740bdd5dc8b529
CodyBense <codybense@gmail.com>
2026-01-19 18:16:42 -0500

    added ssh-connections script and updated path in binds

---
+++ dotfiles/niri/scripts/ssh-connections.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+FILE=$HOME/.local/share/ssh-connections.txt
+
+connections=$(cat "${FILE}" | cut --delimiter ',' --fields 1 | rofi -dmenu -p "connections: ") || exit 0
+mapfile -t username < <(cat "${FILE}" | cut --delimiter ',' --fields 2) || exit 0
+
+case "${connections}" in
+    vault)
+        kitty --title "${connections}" --app-id ssh ssh "${username[0]}"@"${connections}"
+        ;;
+    pikachu)
+        kitty --title "${connections}" --app-id ssh ssh "${username[1]}"@"${connections}"
+        ;;
+    192.168.1.129)
+        kitty --title "${connections}" --app-id ssh ssh "${username[2]}"@"${connections}"
+        ;;
+    *)
+        notify-send "Not a valid ssh connection"
+        ;;
+
+esac
