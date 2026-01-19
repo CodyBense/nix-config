65e31d4832aa48bd8e3c12fefa8f06e4e4dbbabf
CodyBense <codybense@gmail.com>
2026-01-19 17:47:07 -0500

    added packages

--- hosts/pikachu/configuration.nix
+++ hosts/pikachu/configuration.nix
@@ -64,10 +64,13 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
+    wget
     neovim
     git
+    fzf
+    starship
+    direnv
+    zoxide
   ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
