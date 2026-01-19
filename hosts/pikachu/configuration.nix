e69ef85470082e153831c3794dc1f12c368e50cf
CodyBense <codybense@gmail.com>
2026-01-19 17:33:52 -0500

    format and enabled ssh

--- hosts/pikachu/configuration.nix
+++ hosts/pikachu/configuration.nix
@@ -64,10 +64,10 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  neovim
-  git
+    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    #  wget
+    neovim
+    git
   ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -83,7 +83,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
