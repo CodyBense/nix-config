9e14ae83976797429642881b2943c1867bac8a0d
CodyBense <codybense@gmail.com>
2026-01-19 18:07:15 -0500

    added .bash_profile out of store link

--- modules/home-manager/dotfiles.nix
+++ modules/home-manager/dotfiles.nix
@@ -11,6 +11,7 @@ in
       ".config/kitty".source = mkOutOfStoreSymlink "${configDir}/kitty";
       ".config/doom".source = mkOutOfStoreSymlink "${configDir}/doom";
       ".bashrc".source = mkOutOfStoreSymlink "${configDir}/bash/.bashrc";
+      ".bash_profile".source = mkOutOfStoreSymlink "${configDir}/bash/.bash_profile";
       ".config/starship".source = mkOutOfStoreSymlink "${configDir}/startship.toml";
       ".config/rofi".source = mkOutOfStoreSymlink "${configDir}/rofi";
       ".config/zellij".source = mkOutOfStoreSymlink "${configDir}/zellij";
