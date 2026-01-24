4f94f6a3973bd11d7f0fef20997d4c2935d22326
CodyBense <codybense@gmail.com>
2026-01-24 08:44:18 -0500

    added nh and some comments

--- hosts/pikachu/configuration.nix
+++ hosts/pikachu/configuration.nix
@@ -76,6 +76,15 @@
     zoxide
   ];
 
+  # nh
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clien.extraArgs = "--keep-since 4d --keep 5";
+    flake = "/home/${username}/nix-config";
+  };
+
+  # enables flakes
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
