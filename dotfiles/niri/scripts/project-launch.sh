d4d01e1884743571d1c237933c89128d1ed86144
CodyBense <codybense@gmail.com>
2026-01-23 19:26:24 -0500

    updated project launch script

--- dotfiles/niri/scripts/project-launch.sh
+++ dotfiles/niri/scripts/project-launch.sh
@@ -11,32 +11,16 @@ check_project_session() {
 }
 
 DIR=$HOME/workspaces/github/CodyBense
-other_projects="new\ndotfiles"
+other_projects="nix-config\n"
 options=$(printf "${other_projects}\n$(ls $DIR)\n" | rofi -dmenu -p "Projects: ")
 
 case "${options}" in
-    "dotfiles")
-        kitty --title "$options" --app-id project --directory $HOME/dotfiles zellij a -c dotfiles &
-        emacsclient -c $HOME/dotfiles/
-        ;;
-    "new")
-        project_name="$(printf '' | rofi -dmenu -p 'Project Name: ')"
-        [ -n "$project_name" ] || exit 0
-        echo $DIR/$project_name
-        mkdir -p $DIR/$project_name
-        kitty --title "$options" --app-id project --directory $DIR/$project_name zellij a -c $project_name &
-        emacsclient -c $DIR/$project_name
-        ;;
-    *)
-        # kitty --title "$options" --app-id project --directory $DIR/$options zellij a -c $options &
-        # emacsclient -c $DIR/$options
-        session_name="$(check_project_session $options)"
-        if [[ session_name == "" ]]; then
-            echo "It is not a session"
-            # kitty --title "$options" --app-id project --directory $DIR/$options zellij attach $options &
-        else
-            echo "It is a session"
-            # kitty --title "$options" --app-id project --directory $DIR/$options zellij --session $options &
-        fi
-        ;;
+nix-config)
+    kitty --session "$HOME"/nix-config/launch.kitty-session &
+    emacsclient -n "$HOME"/nix-config
+    ;;
+*)
+    kitty --title "$options" --session "$DIR"/"$options"/launch.kitty-session &
+    emacsclient -n $DIR/$options
+    ;;
 esac
