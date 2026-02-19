b28c0ba2f7c77c85f365475293c5ee95b6d8f955
CodyBense <codybense@gmail.com>
2026-02-19 17:22:00 -0500

    created module for smb shares and imported it to pikachu config

---
+++ modules/shares/mounts.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  fileSystems."/data/vault" = {
+    device = "//vault/data";
+    fsType = "cifs";
+    options =
+      let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in
+      [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
+  };
+}
