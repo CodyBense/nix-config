635645e869101921a87edf8d0164d32b7d070a0a
CodyBense <codybense@gmail.com>
2026-02-18 20:22:37 -0500

    fixed immich

--- modules/server/immich.nix
+++ modules/server/immich.nix
@@ -4,15 +4,11 @@
   pkgs,
   ...
 }:
-let
-  HOME = "/home/cody";
-in
 
 {
   services.immich = {
     enable = true;
     port = 2283;
-    mediaLocation = "${HOME}/Photos/immich";
     host = "0.0.0.0";
     openFirewall = true;
   };
