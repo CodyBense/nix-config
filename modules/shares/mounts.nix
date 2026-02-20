1af884f48892998c3dbd5c10b3d26a6f506cd673
CodyBense <codybense@gmail.com>
2026-02-19 20:18:23 -0500

    fixed smb mount

--- modules/shares/mounts.nix
+++ modules/shares/mounts.nix
@@ -6,7 +6,7 @@
 }:
 
 {
-  fileSystems."/data/vault" = {
+  fileSystems."/mnt/vault" = {
     device = "//vault/data";
     fsType = "cifs";
     options =
@@ -15,6 +15,6 @@
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
       in
-      [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=1000" ];
+      [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100" ];
   };
 }
