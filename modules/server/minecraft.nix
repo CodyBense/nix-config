98b3a2dc21517093796dcf427bbd223c55c9e50b
CodyBense <codybense@gmail.com>
2026-02-22 13:54:43 -0500

    fixed service error, removed custom dir location

--- modules/server/minecraft.nix
+++ modules/server/minecraft.nix
@@ -6,14 +6,16 @@
     eula = true;
     declarative = true;
 
-    dataDir = "/home/${username}/minecraft-server/vanilla";
+    # default dir is /var/lib/minecraft
+    # dataDir = "/home/${username}/minecraft-server/vanilla";
 
     serverProperties = {
       # default port 25565
       gamemode = "survival";
       difficulty = "hard";
       simulation-distance = 10;
-      level-name = "vanilla";
+      level-seed = "4";
+      # level-name = "vanilla";
     };
   };
 }
