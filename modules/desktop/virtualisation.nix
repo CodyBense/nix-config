0f0d50cded86b2a554f6ad8e244895dfde777aa0
CodyBense <codybense@gmail.com>
2026-03-19 17:46:25 -0400

    added winboat options

--- modules/desktop/virtualisation.nix
+++ modules/desktop/virtualisation.nix
@@ -6,6 +6,12 @@
 }:
 
 {
+  environment.systemPackages = with pkgs; [
+    winboat
+  ];
+
+  virtualisation.docker.enable = true;
+  users.users.cody.extraGroup = [ "docker" ];
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ "cody" ];
 }
