0c3813e51053a64cb81108c6d693095109eec8e8
CodyBense <codybense@gmail.com>
2026-03-21 14:08:33 -0400

    updated so emacs happens second

--- dotfiles/niri/scripts/project-launch-v2.sh
+++ dotfiles/niri/scripts/project-launch-v2.sh
@@ -5,4 +5,5 @@ DIR=$HOME/workspaces/github/CodyBense
 kitty --app-id project-launch --directory ~/workspaces/github/CodyBense -o font_features=none -e bash -c "ls | tv dirs >/tmp/project-selection"
 project=$(cat /tmp/project-selection)
 kitty --title "$project" --session "$HOME"/nix-config/dotfiles/kitty/sessions/project.kitty-session -d "$DIR"/"$project" &
+sleep 0.5
 emacsclient -n "$DIR"/"$project"
