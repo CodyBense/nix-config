60f2623369e118bd7c32624d6e0b3e7629351b94
CodyBense <codybense@gmail.com>
2026-03-21 13:08:20 -0400

    created a new project launch script using television and updated window rules and binds for it

---
+++ dotfiles/niri/scripts/project-launch-v2.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+DIR=$HOME/workspaces/github/CodyBense
+
+kitty --app-id project-launch --directory ~/workspaces/github/CodyBense -o font_features=none -e bash -c "ls | tv dirs >/tmp/project-selection"
+project=$(cat /tmp/project-selection)
+kitty --title "$project" --session "$HOME"/nix-config/dotfiles/kitty/sessions/project.kitty-session -d "$DIR"/"$project" &
+emacsclient -n "$DIR"/"$project"
