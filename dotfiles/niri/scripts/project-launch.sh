0026560359d2c6ee3816c6eb7cdcfdcb06e2f637
CodyBense <codybense@gmail.com>
2026-03-23 19:30:24 -0400

    formatted

--- dotfiles/niri/scripts/project-launch.sh
+++ dotfiles/niri/scripts/project-launch.sh
@@ -2,7 +2,7 @@
 
 DIR=$HOME/workspaces/github/CodyBense
 
-kitty --app-id project-launch --directory ~/workspaces/github/CodyBense -o font_features=none -e bash -c "ls | tv dirs >/tmp/project-selection"
+kitty --app-id project-launch --directory ~/workspaces/github/CodyBense -o font_features=none -e bash -c "ls | tv dirs > /tmp/project-selection"
 project=$(cat /tmp/project-selection)
 kitty --title "$project" --session "$HOME"/nix-config/dotfiles/kitty/sessions/project.kitty-session -d "$DIR"/"$project" &
 sleep 0.5
