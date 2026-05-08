a7e9c5ad3715fcf721b079fb3601a0293a2eedfa
CodyBense <codybense@gmail.com>
2026-05-08 18:31:54 -0400

    updated postgres config

--- modules/server/databases.nix
+++ modules/server/databases.nix
@@ -9,9 +9,22 @@
   config.services.postgresql = {
     enable = true;
     ensureDatabases = [ "test" ];
+    enableTCPIP = true;
+    # port = 5432;
     authentication = pkgs.lib.mkOverride 10 ''
       #type database DBuser auth-method
       local all      all    trust
+      # ... other auth rules ...
+
+      # ipv4
+      host all all 127.0.0.1/32 trust
+      # ipv6
+      host all all ::1/128 trust
+    '';
+    initialScript = pkgs.writeText "backend-initScript" ''
+      CREATE ROLE nixcloud WITH LOGIN PASSWORD 'nixclould' CREATEDB;
+      CREATE DATABASE nixcloud;
+      GRANT ALL PRIVILEGES ON DATABASE nixcloud TO nixclould;
     '';
   };
 }
