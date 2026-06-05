961bf5913ec1a203ea4365fbc121ef04e3b17b08
CodyBense <codybense@gmail.com>
2026-06-05 16:21:19 -0400

    updated

--- modules/server/ollama.nix
+++ modules/server/ollama.nix
@@ -9,15 +9,13 @@
   services.ollama = {
     enable = true;
     package = pkgs.ollama-cuda;
+    acceleration = "cuda";
     loadModels = [
       "llama3.2:3B"
       "deepseek-r1:1.5b"
     ];
   };
 
-  services.open-webui = {
-    enable = true;
-    package = pkgs.open-webui;
-    port = 8083;
-  };
+  services.open-webui.enable = true;
+  # default port 8080
 }
