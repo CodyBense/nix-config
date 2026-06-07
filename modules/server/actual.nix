fab4c944e24bbeb75e65de074f67f7c02106a9a0
CodyBense <codybense@gmail.com>
2026-06-07 07:41:19 -0400

    trying to fix actual and samba

--- modules/server/actual.nix
+++ modules/server/actual.nix
@@ -6,12 +6,39 @@
 }:
 
 {
+  # services.actual = {
+  #   enable = true;
+  #   openFirewall = true;
+  #   settings = {
+  #     port = 3000;
+  #   };
+  # };
+
+  networking.firewall.allowedTCPPorts = [ 3000 ];
   services.actual = {
     enable = true;
     openFirewall = true;
     settings = {
+      hostname = "0.0.0.0";
       port = 3000;
+      dataDir = "/var/lib/actual";
     };
   };
-  networking.firewall.allowedTCPPorts = [ 3000 ];
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."budget.codybense.com" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:3000";
+        proxyWebsockets = true;
+      };
+    };
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "codybense@proton.me";
+  };
 }
--- modules/server/samba.nix
+++ modules/server/samba.nix
@@ -1,11 +1,59 @@
 { config, lib, pkgs, ... }:
 
 {
+  # services.samba = {
+  #   package = pkgs.samba4Full;
+  #   usershares.enable = true;
+  #   enable = true;
+  #   openFirewall = true;
+  # };
+
+  # services.samba-wsdd = {
+  #   enable = true;
+  #   openFirewall = true;
+  # };
+
+  users.users.cody = {
+    isNormalUser = true;
+    extraGroups = [ "samba" ];
+  };
+
   services.samba = {
-    package = pkgs.samba4Full;
-    usershares.enable = true;
     enable = true;
     openFirewall = true;
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "maul";
+        "netbios name" = "maul";
+        security = "user";
+        #"use sendfile" = "yes";
+        #"max protocol" = "smb2";
+        # note: localhost is the ipv6 localhost ::1
+        "hosts allow" = "192.168.1. 127.0.0.1 localhost";
+        "hosts deny" = "0.0.0.0/0";
+        "guest account" = "nobody";
+        "map to guest" = "bad user";
+      };
+      "public" = {
+        "path" = "/mnt/Shares/Public";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "yes";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "cody";
+      };
+      "private" = {
+        "path" = "/mnt/Shares/Private";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "cody";
+      };
+    };
   };
 
   services.samba-wsdd = {
@@ -13,8 +61,16 @@
     openFirewall = true;
   };
 
-  users.users.cody = {
-    isNormalUser = true;
-    extraGroups = [ "samba" ];
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+    publish.enable = true;
+    publish.userServices = true;
+    # ^^ Needed to allow samba to automatically register mDNS records (without the need for an `extraServiceFile`
+    # ^^ Not one hundred percent sure if this is needed- if it aint broke, don't fix it
   };
+
+  networking.firewall.enable = true;
+  networking.firewall.allowPing = true;
 }
