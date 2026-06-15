58a67f25d4392fc404f84f3ef59cb715d867cd50
CodyBense <codybense@gmail.com>
2026-06-14 22:07:56 -0400

    updated audio

--- modules/desktop/audio.nix
+++ modules/desktop/audio.nix
@@ -6,11 +6,11 @@
 }:
 
 {
-  hardware.enableRedistributableFirmware = true;
-  hardware.firmware = with pkgs; [
-    sof-firmware
-    linux-firmware
-  ];
+  # hardware.enableRedistributableFirmware = true;
+  # hardware.firmware = with pkgs; [
+  #   sof-firmware
+  #   linux-firmware
+  # ];
 
   security.rtkit.enable = true;
   services.pulseaudio.enable = false;
@@ -20,29 +20,29 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    jack.enable = true;
-    wireplumber.enable = true;
-    wireplumber.extraConfig = {
-      "monitor.alas" = {
-        "monitor.alsa.rules" = [
-          {
-            matches = [ { "node.name" = "~alsa_output.*"; } ];
-            actions."update-props" = {
-              "audio.format" = "S32LE";
-              "audio.rate" = 48000;
-              "api.alsa.period-size" = 256;
-            };
-          }
-        ];
-      };
-    };
+    # jack.enable = true;
+    # wireplumber.enable = true;
+    # wireplumber.extraConfig = {
+    #   "monitor.alas" = {
+    #     "monitor.alsa.rules" = [
+    #       {
+    #         matches = [ { "node.name" = "~alsa_output.*"; } ];
+    #         actions."update-props" = {
+    #           "audio.format" = "S32LE";
+    #           "audio.rate" = 48000;
+    #           "api.alsa.period-size" = 256;
+    #         };
+    #       }
+    #     ];
+    #   };
+    # };
   };
 
   environment.systemPackages = with pkgs; [
     pavucontrol
-    playerctl
-    wireplumber
-    alsa-utils
-    sof-firmware
+    # playerctl
+    # wireplumber
+    # alsa-utils
+    # sof-firmware
   ];
 }
