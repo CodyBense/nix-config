6d2379b28f8e2dd7ef0301433cfa67a7ea9d0324
CodyBense <codybense@gmail.com>
2026-07-04 10:51:42 -0400

    updated mounts

--- modules/shares/mounts.nix
+++ modules/shares/mounts.nix
@@ -6,8 +6,8 @@
 }:
 
 {
-  fileSystems."/mnt/maul" = {
-    device = "//192.168.1.232/mnt/Shares/Private";
+  fileSystems."/mnt/maul/private" = {
+    device = "//192.168.1.232/private";
     fsType = "cifs";
     options =
       let
@@ -17,4 +17,17 @@
       in
       [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100" ];
   };
+
+  fileSystems."/mnt/maul/public" = {
+    device = "//192.168.1.232/public";
+    fsType = "cifs";
+    options =
+      let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in
+      [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100" ];
+  };
+
 }
