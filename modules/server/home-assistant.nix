243d61ef45482f8b7613843bec52198f38f78640
CodyBense <codybense@gmail.com>
2026-07-05 11:51:02 -0400

    updating home-assistant

--- modules/server/home-assistant.nix
+++ modules/server/home-assistant.nix
@@ -5,24 +5,44 @@
   ...
 }:
 {
-  services.home-assistant = {
+  # services.home-assistant = {
+  #   enable = true;
+  #   config = null;
+  #   lovelaceConfig = null;
+  #   configDir = "/etc/home-assistant";
+  #   extraComponents = [
+  #     "analytics"
+  #     "google_translate"
+  #     "met"
+  #     "radio_browser"
+  #     "default_config"
+  #     "esphome"
+  #     "my"
+  #     "shopping_list"
+  #     "wled"
+  #     "isal"
+  #   ];
+  # };
+
+services.home-assistant = {
     enable = true;
-    config = null;
-    lovelaceConfig = null;
-    configDir = "/etc/home-assistant";
     extraComponents = [
+      # Components required to complete the onboarding
       "analytics"
       "google_translate"
       "met"
       "radio_browser"
-      "default_config"
-      "esphome"
-      "my"
       "shopping_list"
-      "wled"
+      # Recommended for fast zlib compression
+      # https://www.home-assistant.io/integrations/isal
       "isal"
     ];
+    config = {
+      # Includes dependencies for a basic setup
+      # https://www.home-assistant.io/integrations/default_config/
+      default_config = {};
+    };
   };
 
-  networking.firewall.allowedTCPPorts = [ config.services.home-assistant.config.http.server_port ];
+  networking.firewall.allowedTCPPorts = [ 8123 ];
 }
