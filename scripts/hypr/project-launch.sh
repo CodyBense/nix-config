763b6a775b74a6e75b922505b8cfeeaa8d8a2908
CodyBense <codybense@gmail.com>
2026-07-11 13:58:15 -0400

    updated project launch script

--- scripts/hypr/project-launch.sh
+++ scripts/hypr/project-launch.sh
@@ -6,6 +6,4 @@ kitty --app-id project-launch --directory ~/workspaces/github/CodyBense -o font_
 project=$(cat /tmp/project-selection)
 
 # project=$(kitty --app-id project-launch --directory ~/workspaces/github/CodyBense -o font_features=none -e bash -c "ls | tv dirs > /tmp/project-selection")
-kitty --title "$project" --directory ~/workspaces/github/CodyBense/"$project" -e bash -c "zellij attach -c $project"
-sleep 0.5
-emacsclient -n "$DIR"/"$project"
+kitty --title "$project" --directory ~/workspaces/github/CodyBense/"$project" -e bash -c "zellij attach -c $project --layout ~/nix-config/dotfiles/zellij/layout/project.kdl "
