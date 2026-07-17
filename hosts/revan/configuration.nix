5cf30e93ba0ccdc3809bf55fd8a4a0d032c257cc
CodyBense <codybense@gmail.com>
2026-07-17 12:52:25 -0400

    temp removed cachix for hyprland

--- hosts/revan/configuration.nix
+++ hosts/revan/configuration.nix
@@ -85,17 +85,17 @@
     "flakes"
   ];
 
-  nix.settings = {
-    substituters = [ "https://hyprland.cachix.org" ];
-    trusted-substituters = [ "https://hyprland.cachix.org" ];
-    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-    # Required so non-root users are allowed to use the above substituter/keys.
-    # Use @wheel for all sudo users, or list your username explicitly.
-    trusted-users = [
-      "root"
-      "@wheel"
-    ];
-  };
+  # nix.settings = {
+  #   substituters = [ "https://hyprland.cachix.org" ];
+  #   trusted-substituters = [ "https://hyprland.cachix.org" ];
+  #   trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  #   # Required so non-root users are allowed to use the above substituter/keys.
+  #   # Use @wheel for all sudo users, or list your username explicitly.
+  #   trusted-users = [
+  #     "root"
+  #     "@wheel"
+  #   ];
+  # };
 
   # for helium browser install
   security.chromiumSuidSandbox.enable = true;
