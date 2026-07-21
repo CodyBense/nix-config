22a9f672ec1360acf136a7f36b5407e5bd479e71
CodyBense <codybense@gmail.com>
2026-07-21 18:49:52 -0400

    added cachix for hyprland

--- hosts/revan/configuration.nix
+++ hosts/revan/configuration.nix
@@ -201,6 +201,16 @@
     flake = "/home/${username}/nix-config";
   };
 
+  nix.settings = {
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    trusted-users = [
+      "root"
+      "@wheel"
+    ];
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
