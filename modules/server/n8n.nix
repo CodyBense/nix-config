78d82433db5670ae3cce0f98eee5fdaa222ee033
CodyBense <codybense@gmail.com>
2026-07-26 07:09:31 -0400

    fixing errors

--- modules/server/n8n.nix
+++ modules/server/n8n.nix
@@ -8,10 +8,10 @@
   services.n8n = {
     enable = true;
     openFirewall = true;
-  };
-  environment = {
-    N8N_PORT = "5678";
-    N8N_HOST = "0.0.0.0";
+    environment = {
+      N8N_PORT = "5678";
+      N8N_HOST = "0.0.0.0";
+    };
   };
 
   networking.firewall.allowedTCPPorts = [ 5678 ];
